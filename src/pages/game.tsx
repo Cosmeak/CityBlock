3a48b3ddacdd8330cf7706349cae6e93d64b5c0c
Guillaume <guillaume.fine@edu.devinci.fr>
2021-11-22 10:26:38 +0100

    :hammer: Update game vue

--- src/pages/game.tsx
+++ src/pages/game.tsx
@@ -1,26 +1,52 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonInput, IonButton, IonLabel, IonItem, IonCheckbox } from '@ionic/react';
+import React from 'react';
+import { IonPage, IonTitle, IonLabel, IonTabs, IonTabBar, IonTabButton, IonTab, IonIcon, IonRouterOutlet } from '@ionic/react';
+import { Route, Redirect } from 'react-router-dom';
+import {  personCircle, map, informationCircle } from 'ionicons/icons';
 import '../css/style.css';
 
 const Game: React.FC = () => {
     return (
         <IonPage>
-            <IonTitle>Game</IonTitle>
-        </IonPage>
-    );
-};
+            
+            {/* Content of tab info */}
+            <IonTab tab="info">
+                <h1>Info</h1>
+            </IonTab>
 
-const Info: React.FC = () => {
-    return (
-        <IonPage>
-            <IonTitle>Information</IonTitle>
-        </IonPage>
-    );
-};
+            {/* Content of tab board (main vue) */}
+            <IonTab tab="board">
 
-const Rules: React.FC = () => {
-    return (
-        <IonPage>
-            <IonTitle>Rules</IonTitle>
+            </IonTab>
+
+            {/* Page of all game rules */}
+            <IonTab tab="rules">
+
+            </IonTab>
+
+            {/* Navigation tab (bottom of screen) */}
+            <IonTabs>
+                <IonRouterOutlet>
+                    <Route path="/"></Route>
+                </IonRouterOutlet>
+                <IonTabBar slot="bottom">
+                
+                    <IonTabButton tab="Me">
+                        <IonIcon icon={personCircle} />
+                        <IonLabel>Me</IonLabel>
+                    </IonTabButton>
+                
+                    <IonTabButton tab="Board">
+                        <IonIcon icon={map} />
+                        <IonLabel>Board</IonLabel>
+                    </IonTabButton>
+                
+                    <IonTabButton tab="Rules">
+                        <IonIcon icon={informationCircle} />
+                        <IonLabel>Rules</IonLabel>
+                    </IonTabButton>
+                </IonTabBar>
+            </IonTabs>
+
         </IonPage>
     );
 };
