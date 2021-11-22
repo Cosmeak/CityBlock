6a27596a281cc1be5bfcbd741efa0a40264002e8
Guillaume <guillaume.fine@edu.devinci.fr>
2021-11-22 11:12:02 +0100

    :hammer: Update game vue

--- src/pages/game.tsx
+++ src/pages/game.tsx
@@ -1,54 +1,54 @@
 import React from 'react';
 import { IonPage, IonTitle, IonLabel, IonTabs, IonTabBar, IonTabButton, IonTab, IonIcon, IonRouterOutlet } from '@ionic/react';
 import { Route, Redirect } from 'react-router-dom';
-import {  personCircle, map, informationCircle } from 'ionicons/icons';
+import { personCircle, map, informationCircle } from 'ionicons/icons';
 import '../css/style.css';
 
 const Game: React.FC = () => {
-    return (
-        <IonPage>
-            
-            {/* Content of tab info */}
-            <IonTab tab="info">
-                <h1>Info</h1>
-            </IonTab>
+	return (
+		<IonPage>
 
-            {/* Content of tab board (main vue) */}
-            <IonTab tab="board">
+			{/* Content of tab info */}
+			<IonTab tab="info">
+				<h1>Info</h1>
+			</IonTab>
 
-            </IonTab>
+			{/* Content of tab board (main vue) */}
+			<IonTab tab="board">
 
-            {/* Page of all game rules */}
-            <IonTab tab="rules">
+			</IonTab>
 
-            </IonTab>
+			{/* Page of all game rules */}
+			<IonTab tab="rules">
 
-            {/* Navigation tab (bottom of screen) */}
-            <IonTabs>
-                <IonRouterOutlet>
-                    <Route path="/"></Route>
-                </IonRouterOutlet>
-                <IonTabBar slot="bottom">
-                
-                    <IonTabButton tab="Me">
-                        <IonIcon icon={personCircle} />
-                        <IonLabel>Me</IonLabel>
-                    </IonTabButton>
-                
-                    <IonTabButton tab="Board">
-                        <IonIcon icon={map} />
-                        <IonLabel>Board</IonLabel>
-                    </IonTabButton>
-                
-                    <IonTabButton tab="Rules">
-                        <IonIcon icon={informationCircle} />
-                        <IonLabel>Rules</IonLabel>
-                    </IonTabButton>
-                </IonTabBar>
-            </IonTabs>
+			</IonTab>
 
-        </IonPage>
-    );
+			{/* Navigation tab (bottom of screen) */}
+			<IonTabs>
+				<IonRouterOutlet>
+					<Route path="/"></Route>
+				</IonRouterOutlet>
+				<IonTabBar slot="bottom">
+
+					<IonTabButton tab="Me">
+						<IonIcon icon={personCircle} />
+						<IonLabel>Me</IonLabel>
+					</IonTabButton>
+
+					<IonTabButton tab="Board">
+						<IonIcon icon={map} />
+						<IonLabel>Board</IonLabel>
+					</IonTabButton>
+
+					<IonTabButton tab="Rules">
+						<IonIcon icon={informationCircle} />
+						<IonLabel>Rules</IonLabel>
+					</IonTabButton>
+				</IonTabBar>
+			</IonTabs>
+
+		</IonPage>
+	);
 };
 
 export default Game;
