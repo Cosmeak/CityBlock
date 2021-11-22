36d78f41b51b7f9ef7375580755143313b41cf54
Steven <Steven.madi@edu.devinci.fr>
2021-11-22 23:46:00 +0100

    update SCSS login page :lipstick:

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,29 +1,51 @@
-import { IonHeader, IonPage, IonTitle, IonInput, IonButton, IonLabel, IonItem, IonContent, IonToolbar } from '@ionic/react';
+import { IonHeader, IonPage, IonTitle, IonInput, IonButton, IonLabel, IonItem, IonContent, IonToolbar, IonThumbnail, IonImg } from '@ionic/react';
 import '../css/style.css';
 
+type Item = {
+  src: string;
+};
+
+type bg = {
+  src: string;
+};
+
+const bg: bg[] = [{ src: 'assets/img/bg-triangle.png'}];
+
+const items: Item[] = [{ src: 'assets/img/LogoCityBlockVectorisation.png'}];
 const Login: React.FC = () => {
   return (
     <IonPage>
-
+      
       {/* Header of the page */}
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Choose your name</IonTitle>
-        </IonToolbar>
-      </IonHeader>
 
       {/* Content of the page */}
       <IonContent>
-        <form className="ion-padding">
-          <IonItem>
-            <IonLabel position="floating">Username</IonLabel>
-            <IonInput /> {/* For enter your pseudo */}
+      {bg.map((image) => (
+          <IonThumbnail className="h-w">
+            <IonImg src={image.src} />
+          </IonThumbnail>
+      ))}
+      {items.map((image) => (
+          <IonThumbnail className="margin-center-horizontal h-w-300 absolute border5px">
+            <IonImg src={image.src} />
+          </IonThumbnail>
+      ))}
+        <form className="ion-padding absolute border5px ">
+          <IonItem className='pseudo margin-center-horizontal border5px'>
+            <IonLabel position='fixed' >Pseudo</IonLabel>
+            <IonInput></IonInput> {/* For enter your pseudo */}
           </IonItem>
-          <IonButton className="ion-margin-top" type="submit" expand="block" >
-            Login
+          <IonButton className="margin-center-horizontal button border5px" fill='clear' type="submit" expand="block" >
+            Entrer dans le jeu
+          </IonButton>
+          <IonButton className="admin margin-center-horizontal " fill='clear' type="submit" expand="block" >
+            Administrateur ?
           </IonButton>
         </form>
+        
       </IonContent>
+      
+
       
     </IonPage>
   );
